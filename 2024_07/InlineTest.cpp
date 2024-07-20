627371ae6ca693fed428e0a2b2ab05fd18af9238
DeyDeyDream <2677933891@qq.com>
2024-07-20 17:33:53 +0800

    内联函数的声明定义不能分离 如果有需要的场景可以直接在头文件中书写内联函数

---
+++ 2024_07/InlineTest.cpp
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+void _F(int val) {
+	std::cout << val << std::endl;
+}
+//inline void F(int val) {
+//	std::cout << val << std::endl;
+//}
---
+++ 2024_07/InlineTest.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+
+void _F(int val);
+inline void F(int val) {
+	std::cout << val << std::endl;
+}
