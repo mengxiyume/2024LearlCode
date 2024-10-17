5226a2877f0f8f2a70fcb8f17419a82933096127
梦晞 <2677933891@qq.com>
2024-10-17 17:00:00 +0800

    成员访问函数

--- C++NotepadCode/C++笔记5_STL/容器/vector.h
+++ C++NotepadCode/C++笔记5_STL/容器/vector.h
@@ -2,20 +2,35 @@
 
 #include <algorithm>
 #include <cstddef>
+#include <cstring>
+#include <cassert>
 namespace emansis {
 
 	template<class T>
 	class vector {
 	public:
+#pragma region 成员函数
 #pragma region 构造相关重载
-		vector(size_t size = 0)
+		vector()
 		: m_aData(nullptr)
 		, m_pStart(nullptr)
 		, m_pFinish(nullptr)
-		, m_pEndOfStorage(nullptr) { }
+		, m_pEndOfStorage(nullptr) {;}
+		vector(T x) {
+
+		}
+		vector(const vector<T>& src) {
+			vector<T> temp(src);
+			swap(*this, temp);
+		}
 		~vector() {
 			if (m_aData)
 				delete[] m_aData;
+		}
+#pragma endregion
+		vector<T>& operator=(vector<T> v) {
+			swap(*this, v);
+			return *this;
 		}
 #pragma endregion
 #pragma region 迭代器相关
@@ -31,11 +46,74 @@ namespace emansis {
 			return m_pStart;
 		}
 		const_iterator cend() const {
-			return m_pFinish
+			return m_pFinish;
 		}
 #pragma endregion
 #pragma region 空间管理
+		size_t size() {
+			return m_pFinish - m_pStart;
+		}
+		size_t capacity() {
+			return m_pEndOfStorage - m_pStart;
+		}
+		bool empty() {
+			return !(m_pFinish - m_pStart);
+		}
+		void resize(size_t size, T value = 0) {
+			//TODO:resize实现
 
+		}
+		void reserve(size_t newCapacity) {
+			//没有空间时直接申请空间并初始化
+			T* temp = new T[newCapacity];
+			if (m_aData != nullptr) {
+				//已有空间时创建新空间并移动数据和释放旧空间
+				//移动数据
+				memmove(temp, m_aData, size());
+				//释放旧空间
+				delete[] m_aData;
+			}
+			//管理数据移动
+			m_pEndOfStorage	= temp + capacity();
+			m_pFinish		= temp + size();
+			m_pStart		= temp;
+			m_aData			= temp;
+		}
+#pragma endregion
+#pragma region 成员访问
+		T& operator[](size_t pos) {
+			return at(pos);
+		}
+		T& at(size_t pos) {
+			assert(pos < size());
+			return m_aData[pos];
+		}
+		T& front() {
+			return *m_pStart;
+		}
+		T& back() {
+			return *m_pFinish;
+		}
+		T* data() {
+			return m_aData;
+		}
+		//访问const成员变量
+		const T& operator[](size_t pos) const {
+			return at(pos);
+		}
+		const T& at(size_t pos) const {
+			assert(pos < size());
+			return m_aData[pos];
+		}
+		const T& front() const {
+			return *m_pStart;
+		}
+		const T& back() const {
+			return *m_pFinish;
+		}
+		const T* data() const {
+			return m_aData;
+		}
 #pragma endregion
 	private:
 		T* m_aData;			//数据存放的位置
